42c15f71b9feb2ceda5ccb6b8200279a10c789ce
keerthana <keerthananeeluru2001@gmail.com>
2023-10-07 13:09:15 +0530

    database query

---
+++ productdatabase.sql
@@ -0,0 +1,49 @@
+
+
+INSERT INTO ProductDetails (ProductUrl, ProductName, ProductDescription, ProductInStock, ProductPrice) 
+VALUES 
+('https://5.imimg.com/data5/SELLER/Default/2023/2/MR/YA/WM/185331360/advil-pm-tablet.jpg', 'Advil', 'Relieves pain and discomfort which reduces or relieve headaches and sore muscles and pains', 100, 19.99),
+('https://img.medscapestatic.com/pi/features/drugdirectory/octupdate/RBK00561.jpg', 'Mucinex', 'Effective cough relief a medicated syruplike fluid usually flavored and nonnarcotic for relieving coughs', 150, 17.99),
+('https://wellonapharma.com/admincms/product_img/product_resize_img/doxycycline-hyclate-tablets_1632919806.jpg','Doxycycline', 'Fight against bacterial infection in people kills bacteria to grow and multiply', 112, 20.99),
+('https://c8.alamy.com/comp/H2NNCR/stock-photo-of-a-box-of-cetirizine-hydrochoride-antihistamine-tablets-H2NNCR.jpg', 'Cetirizine', 'Prevents and treats allergy symptoms and treats allergic rhinitis and urticaria', 180, 12.99),
+('https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/6dec63a4-f45d-4e87-813a-6c0cb49aba97.jpeg', 'Ferrous Ascorbate', 'Treat and Prevent anaemia when the amount of iron taken in from the diet is not enough', 100, 15.99),
+('https://media.npr.org/assets/img/2022/05/09/gettyimages-1238868405_slide-c8357f0ce7718bb9c7b7da3a9e823c8c860d4f86-s1100-c50.jpg', 'Paxlovid', 'Fight against bacterial infection in people by killing the bacteria', 112, 20.99),
+('https://www.asterisklabs.in/wp-content/uploads/2018/09/LABTEL-40.jpg', 'Telmisartan', 'Medicine can keep blood pressure at a healthy level by causing our body to get rid of water', 112, 20.99),
+('https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/ko6rsu9xwrdb7hrmmszr.jpg', 'Dolo', 'To reduce fever and treat mild to moderate pain which comes under non-steroidal anti-inflammatory drug class', 108, 12.00),
+('https://5.imimg.com/data5/SELLER/Default/2022/8/CW/BB/DS/129887935/paracetamol-tablets-500-mg-500x500.jpeg', 'Paracetamol', ' Used medicine that can treat pain and reduce a high temperature which relieve mild or moderate pain', 100, 10.08),
+('https://5.imimg.com/data5/SELLER/Default/2022/9/FT/AS/TY/132158408/amoxicillin-tablets-500-mg-500x500.jpg', 'Amoxicillin', ' It is penicillin antibiotic that fights bacteria used to treat infection caused by bacteria like bronchitis', 112, 20.99),
+('https://5.imimg.com/data5/SELLER/Default/2022/2/BF/WR/AB/122822982/benzonatate-100mg--500x500.jpg', 'Benzonatate', ' It is a non-narcotic cough medicine works by numbing the throat and lungs making the cough reflex', 100, 40.00),
+('https://images.apollo247.in/pub/media/catalog/product/B/R/BRI0389_1_1.jpg', 'Brilinta', 'Brilinta prevents in your blood from sticking together to form an blood clot that block an artery', 150, 30.85),
+('https://5.imimg.com/data5/SELLER/Default/2022/9/EE/NQ/SR/154492060/ciprofloxacin-tablets-i-p-500-mg.png', 'Ciprofloxacin', 'It is a fluoroquinolone antibiotic used to treat different types of bacterial infections', 102, 23.86),
+('https://img.medscapestatic.com/pi/features/drugdirectory/octupdate/00206176.jpg', 'Entyvio', 'Infusion is used in adults with ulcerative colitis works by preventing too many WBC', 112, 40.99),
+('https://mimsshst.blob.core.windows.net/drug-resources/SG/packshot/Forxiga6001PPS0.JPG', 'Farxiga', 'It is an oral medication that may be given to people with diabetes heart and kidney disease to improve outcomes', 162, 40.99),
+('https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2022/01/effects_-gabapentin_GettyImages1348724431_Header-732x549.jpg', 'Gabapentin', 'medicine used to treat partial seizure nerve pain shingles and restless leg syndrome', 140, 13.50),
+('https://www.drugs.com/images/pills/custom/pill32860-3/humira.jpeg', 'Humira', 'Humira is a tumor necrosis factor blocker that reduces the effect of a substance in the body that can cause inflammation', 112, 20.99),
+('https://tufarmaceuticodeguardia.org/wp-content/uploads/2021/04/IMG_0045-1024x768.jpg', 'Imbruvica', ' It is a oral targeted treatment that may be used alone or in combinations to treat adults with the blood cancer', 140, 50.99),
+('https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/ruysuhy4kzfxvfit7r4g.jpg', 'Januvia', 'Oral diabetes medicine that helps control blood sugar levels regulates the level of insulin your body produces after eating', 132, 15.78),
+('https://www.grxstatic.com/d4fuqqd5l3dbz/products/tms/Package_28085.JPG', 'Kevzara', 'Reduces the effect of a substances in the body that uses inflammation to treat moderate to severe arthritis in adults' , 112, 20.99),
+('https://5.imimg.com/data5/SELLER/Default/2023/9/340551268/MD/YR/QL/194850557/6de7fdab3b6a7e.jpeg', 'Leqvio', 'Prescription medicine used to lower LDL-C cholesterol level for adults with certain types', 112, 20.99),
+('https://c8.alamy.com/zooms/9/64257f2f3dbc4029865c00800174e9f4/d961r1.jpg', 'Lyrica', 'Originally approved FDA approved as an anti-epileptic drug also called an anticonvulsant', 112, 20.99),
+('https://5.imimg.com/data5/SELLER/Default/2022/8/UQ/UQ/HD/23618296/sleepose-xtra-melatonin-tagar-tablets-.jpg', 'Melatonin', 'Natural hormone your body secretes that helps to maintain your wake-sleep cycle called biological clock', 132, 40.99),
+('https://5.imimg.com/data5/SELLER/Default/2021/8/FV/OV/AE/3184985/buprenorphine-0-4-mg-naloxone-0-1-mg.jpg', 'Naloxone', 'FDA approved medicine used to quickly reverse an opioid overdose', 153, 50.99),
+('https://5.imimg.com/data5/SELLER/Default/2023/6/319756639/TV/GZ/IO/191877708/diabetic-mounjaro-medication-for-weight-loss.jpg', 'Mounjaro', 'Used for two types of diabetes in adults to lower blood sugar levels and A1C levels' , 162, 45.99),
+('https://5.imimg.com/data5/SELLER/Default/2021/6/UJ/BK/UX/32935872/pantoprazole-tablets-ip-40-mg.jpeg', 'Pantoprazole', 'Proton pump inhibitor that decreases the amount of acid produced in stomach', 112, 20.99),
+('https://5.imimg.com/data5/SELLER/Default/2021/9/DE/RQ/OR/95089271/0-50mg-clonazepam-dispersible-tablet.jpeg', 'Clonazepam', 'It is a benzodiazepine which thought that benzodiazepines work by enhancing the activity in brain', 112, 20.99),
+('https://5.imimg.com/data5/SELLER/Default/2022/6/OV/EI/QD/49793341/invokana-100mg-canagliflozin-100mg-.jpg', 'Invokana', 'Oral diabetes medicine that helps control blood sugar levels by helping kidneys get rid of glucose from bloodstream', 172, 25.99),
+('https://images.everydayhealth.com/images/dupixent-approved-children-6-mo-and-up-atopic-dermatitis-1440x810.jpg?sfvrsn=f8af23af_1', 'Dupixent' , 'Used in adults and children and older to treat moderate-to serve the dermatitis with eczema', 200,80.67),
+('https://mimsshst.blob.core.windows.net/drug-resources/MY/packshot/Rybelsus6001PPS0.JPG', 'Rybelsus', 'Tablets are used for adults with type 2 diabetes mellitus to control blood level sugars', 250,30.00),
+('https://www.drugs.com/images/pills/custom/pill32933-1/sublocade.jpeg', 'Sublocade', 'Used to treat opioid dependence in people who have received buccal or sublingual at least 7 days', 255,45.00),
+('https://5.imimg.com/data5/SELLER/Default/2022/10/WW/ML/MD/25358643/viagra-tablets.jpg', 'Viagra', 'Relaxes muscles found in the walls of blood vessels and increase blood flow to particular parts of body', 130,23.54),
+('https://5.imimg.com/data5/SELLER/Default/2022/11/EV/YD/VS/23808697/300-mg-wellbutrin-tablet.JPG', 'Wellbutrin', 'Antidepressant medicine that is thought to work in the brain and nerves on the chemical messenger', 100, 24.10),
+('https://5.imimg.com/data5/SELLER/Default/2022/11/ZG/DJ/CU/107184427/70db6bde-3ad3-47b1-bd37-ae636b298059-500x500.webp', 'Acetaminophen', 'Pain reliever and a fever reducer to treat mild to moderate pain and reduce fever', 127,10.00),
+('https://5.imimg.com/data5/SELLER/Default/2021/9/ZA/KF/UI/92728522/xanax-0-5mg-1-.jpg', 'Xanax', 'Benzodiazepine thought that alprazolam works by enhancing the activity of certain neurotransmitter in the brain',98,89.99),
+('https://www.kauverymeds.com/uploads/product/main/1633681578_134125_1.png', 'Ativan', 'Belongs to class of medications called benzodiazepines enhancing the activity of brain',100,30.99),
+('https://5.imimg.com/data5/SELLER/Default/2023/1/GJ/JG/QJ/180746143/otezla-apremilast-tablets.jpg', 'Otezla', 'It is a tablet taken twice a day that is used to lower inflammation in treatment of disease', 125,98.99),
+('https://5.imimg.com/data5/SELLER/Default/2023/7/330471189/RO/NG/SB/192024177/diphenhydramine-hydrochloride-tablet.jpg', 'Benadryl', 'Classified as an antihistamine help relieve symptoms of heavy fever and allergies',109,12.99);
+
+
+
+insert into ProductDetails  (ProductUrl, ProductName, ProductDescription, ProductInStock, ProductPrice) 
+VALUES ('https://5.imimg.com/data5/EG/ML/LF/SELLER-85835045/ryzodeg-flextouch-500x500.jpg','Ascoril LS Syrup','Ascoril LS Syrup i It also provides relief from runny nose, sneezing, itching, and watery eyes.',98,110),
+		('https://newassets.apollo247.com/pub/media/catalog/product/a/l/alk0026_1_feb23.jpg','Alkasol Oral Solution','Alkasol Oral Solution is a medicine used in the treatment of gout and kidney stones',77,208)
+
