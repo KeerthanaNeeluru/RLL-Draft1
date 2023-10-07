a68cb9f8ee6cd199696c7ea927f3888f1cfd1778
keerthana <keerthananeeluru2001@gmail.com>
2023-10-07 13:22:56 +0530

    database feedback query

---
+++ feedbackdatabase.sql
@@ -0,0 +1,11 @@
+USE [S1RLLPharmacyDB]
+GO
+
+
+
+insert into feedbacks values ('Ella','ella@gmail.com','Good app. I like it. As my experience perceived, some services are good and sufficient, like speed of delivery , return and quick refund system policy. But customer phone helpline service is not available, which is needful. The chat brief and short massaging customer services are unsatisfactory and unable to solve any problem solution zone.'),
+							('Evan','evan@gmail.com','Superb application, for medication. It provide High quality medication at lowest price'),
+							('Sharma','sharma@gmail.com','Great experience. On time delivery. Much Better rates than other apps.'),
+							('Sonia','sonia@gmail.com','Its is a nicee app.. Impressive fast delivered before delivery date medicine are all in good case nice saftey packaging..')
+
+
